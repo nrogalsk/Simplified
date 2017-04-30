834ca17138285cc6cdac79115327956b2700d196
Niki <nrogalsk@deakin.edu.au>
2017-04-30 12:13:28 +1000

    Add ability to send an email message

--- app/src/main/java/com/example/niki/simplified/EmailActivity.java
+++ app/src/main/java/com/example/niki/simplified/EmailActivity.java
@@ -1,13 +1,54 @@
 package com.example.niki.simplified;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class EmailActivity extends AppCompatActivity {
+public class EmailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Declaring EditText
+    private EditText editTextEmail;
+    private EditText editTextSubject;
+    private EditText editTextMessage;
+
+    //Send button
+    private Button buttonSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_email);
+        setContentView(R.layout.activity_main);
+
+        //Initialising the views
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextSubject = (EditText) findViewById(R.id.editTextSubject);
+        editTextMessage = (EditText) findViewById(R.id.editTextMessage);
+
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+
+        //Adding OnclickListener when SendButton is pressed
+        buttonSend.setOnClickListener(this);
+    }
+
+
+    private void sendEmail() {
+        //Capture content for the email
+        String email = editTextEmail.getText().toString().trim();
+        String subject = editTextSubject.getText().toString().trim();
+        String message = editTextMessage.getText().toString().trim();
+
+        //Creating SendMail object
+        SendEmail sendMail = new SendEmail(this, email, subject, message);
+
+        //Executing SendEmail to send email
+        sendMail.execute();
+    }
+
+    @Override
+    public void onClick(View v) {
+        sendEmail();
     }
 }
+
---
+++ app/src/main/java/com/example/niki/simplified/EmailConfig.java
@@ -0,0 +1,11 @@
+package com.example.niki.simplified;
+
+/**
+ * Created by niki on 30/4/17.
+ */
+
+public class EmailConfig {
+    public static final String EMAIL ="Enter Gmail Username";
+    public static final String PASSWORD ="Your Password";
+}
+
